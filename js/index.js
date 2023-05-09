83a7a3c9bcbb754e6d8fc9db41731fb33845cb0e
Sean-D-231121 <231121@virtualwindow.co.za>
2023-05-09 21:23:44 +0200

    Finish all tasks

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const name = "Sean";
+const surname = "Dubbelman";
+let age = 21;
+let subject = "interactive development"
+const message = "Hello, My name is "+ name + " " + surname +", I am 21 years old, I am studying " + subject
+
+console.log(message)
+
+let numOne = 10;
+let numTwo = 20;
+let numThree = 5;
+let numFour = 10;
+let answer = 0;
+answer = numOne + (numTwo / numThree) * numFour;
+
+console.log(answer);
+
+let timmyCash = 10;
+let susanCash = 25;
+let timmyTax = 0.15;
+let susanTax = 0.26;
+let Value = 0;
+Value= timmyCash * timmyTax + susanCash * susanTax;
+console.log("The tax man's claim "+ Value + " from Timmy and Susan")
+
+
+let johnMark = 50;
+let timMark = 64;
+let alfieMark = 72;
+let gwenMark = 80;
+let studentAverage = (johnMark + timMark + alfieMark + gwenMark) / 4
+console.log("The student average is: " + studentAverage + "/100")
+
+johnMark , timMark, alfieMark, gwenMark += 7;
+studentAverage = (johnMark + timMark + alfieMark + gwenMark) / 4;
+
+console.log("The student average is: " + studentAverage +"/100");
+
+
+
+
+
+
